843d7a3778ac97a66f078219923448f2ce5adc45
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-06-24 18:26:04 -0300

    Added Fonts and colors

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,7 +3,6 @@ import {
   View,
   Text,
   Image,
-  StatusBar
 } from "react-native";
 
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -13,27 +12,21 @@ import { styles } from './styles';
 export function SignIn(){
   return (
     <View style={styles.container}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-
       <Image
         source={IllustrationImg}
         style={styles.image}
-        resizeMode="cover"
+        resizeMode="stretch"
       />
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se{'\n'}
+          e organize suas{'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
         
