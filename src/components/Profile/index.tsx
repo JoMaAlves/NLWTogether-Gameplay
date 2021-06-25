121afbd8330f0af6278cf27d8ba9bba3bb5116d6
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-06-24 21:54:24 -0300

    Small fix

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
 
-import { Avatar } from '../Avatar'
+import { Avatar } from '../Avatar';
 
-import { styles } from './styles'
+import { styles } from './styles';
 
 export function Profile() {
   return (
@@ -28,5 +28,5 @@ export function Profile() {
       </View>
       
     </View>
-  )
+  );
 }
