c2adef72fb208c282b35a8a8b78dafb813bf34fe
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-06-26 16:54:22 -0300

    Finished AppointmentDetails Page

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { ImageBackground, View, Text, FlatList } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
+import { ListDivider } from '../../components/ListDivider';
 import { Background } from "../../components/Background";
+import { ListHeader } from "../../components/ListHeader";
+import { ButtonIcon } from "../../components/ButtonIcon";
 import { Header } from '../../components/Header';
+import { Member } from '../../components/Member';
+
 import { theme } from '../../global/styles/theme';
+import BannerImg from '../../assets/banner.png';
+import { styles } from './styles';
 
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: '1',
+      username: 'Jomassauro',
+      avatar_url: 'https://github.com/JoMaAlves.png',
+      status: 'online'
+    },
+    {
+      id: '2',
+      username: 'Mr.Orange',
+      avatar_url: 'https://github.com/DuduSouzaVeiga.png',
+      status: 'offline'
+    },
+  ]
   return (
     <Background>
       <Header 
@@ -22,6 +43,41 @@ export function AppointmentDetails() {
           </BorderlessButton>
         }
       />
+
+      <ImageBackground
+        source={BannerImg}
+        style={styles.banner}
+      >
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>
+            Lendários
+          </Text>
+
+          <Text style={styles.subtitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+          </Text>
+        </View>
+
+      </ImageBackground>
+
+      <ListHeader
+        title="Jogadores"
+        subtitle="Total: 2"
+      />
+
+      <FlatList 
+        data={members}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Member data={item} />
+        )}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.members}
+      />
+
+      <View style={styles.footer}>
+        <ButtonIcon title="Entrar na partida" />
+      </View>
     </Background>
-  )
+  );
 }
