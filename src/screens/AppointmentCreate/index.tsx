b9eb3186d8213e79d4a889112c4feb3106d0a62c
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-06-26 20:44:25 -0300

    Added the Interaction with ModalView

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -6,18 +6,31 @@ import {
 import { RectButton } from 'react-native-gesture-handler';
 
 import { CategorySelect } from "../../components/CategorySelect";
-import { Background } from "../../components/Background";
+import { ModalView } from "../../components/ModalView";
 import { SmallInput } from '../../components/SmallInput';
 import { GuildIcon } from '../../components/GuildIcon';
 import { TextArea } from '../../components/TextArea';
+import { GuildProps } from '../../components/Guild';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Guilds } from '../Guilds';
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
 export function AppointmentCreate() {
   const [category, setCategory] = useState('');
+  const [openGuildsModal, setOpenGuildsModal] = useState(false);
+  const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+  function handleOpenGuilds() {
+    setOpenGuildsModal(true);
+  }
+
+  function handleGuildSelect(guildSelected: GuildProps) {
+    setGuild(guildSelected)
+    setOpenGuildsModal(false);
+  }
   
   return (
     <KeyboardAvoidingView
@@ -45,16 +58,21 @@ export function AppointmentCreate() {
         />
 
         <View style={styles.form}>
-          <RectButton>
+          <RectButton onPress={handleOpenGuilds}>
             <View style={styles.select}>
               {
-                //<View style={styles.image}/>
-                <GuildIcon />
+                guild.icon
+                ? <GuildIcon /> 
+                : <View style={styles.image}/>
               }
 
               <View style={styles.selectBody}>
                 <Text style={styles.label}>
-                  Selecione um servidor
+                  {
+                    guild.name
+                    ? guild.name 
+                    : "Selecione um servidor" 
+                  }
                 </Text>
               </View>
 
@@ -118,6 +136,10 @@ export function AppointmentCreate() {
           </View>
         </View>
       </ScrollView>
+
+      <ModalView visible={openGuildsModal}>
+        <Guilds handleGuildSelect={handleGuildSelect}/>
+      </ModalView>
     </KeyboardAvoidingView>
   );
 }
--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
+import { GuildProps } from '../../components/Guild';
 
 import { Guild } from '../../components/Guild';
 import { ListDivider } from '../../components/ListDivider';
 
 import { styles } from './styles';
 
-export function Guilds() {
+type Props = {
+  handleGuildSelect: (guild: GuildProps) => void;
+}
+
+export function Guilds({ handleGuildSelect }: Props) {
   const guilds = [
     {
       id: '1',
       name: 'Lendários',
-      icon: null,
+      icon: "image.png",
       owner: true
-    }
+    },
+    {
+      id: '2',
+      name: 'Lendários',
+      icon: "image.png",
+      owner: true
+    },
   ];
 
   return (
@@ -22,13 +33,15 @@ export function Guilds() {
         data={guilds}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <Guild data={item} />
+          <Guild
+            data={item}
+            onPress={() => handleGuildSelect(item)}
+          />
         )}
         showsVerticalScrollIndicator={false}
         ItemSeparatorComponent={() => <ListDivider />}
         style={styles.guilds}
       />
-
     </View>
   );
 }
