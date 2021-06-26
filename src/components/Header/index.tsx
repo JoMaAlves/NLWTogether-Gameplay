fd0ce5de5ec8aa9ec9ae064d46830d1aff34b6e5
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-06-26 15:40:58 -0300

    Added Share Button over AppointmentDetails Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -15,11 +16,18 @@ type Props = {
 
 export function Header({ title, action }: Props) {
   const { secondary100, secondary40, heading } = theme.colors;
+
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <LinearGradient style={styles.container}
       colors={[secondary100, secondary40]}
     >
-      <BorderlessButton>
+      <BorderlessButton onPress={handleGoBack}>
         <Feather
           name="arrow-left"
           size={24}
--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { Fontisto } from '@expo/vector-icons';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 import { Background } from "../../components/Background";
 import { Header } from '../../components/Header';
+import { theme } from '../../global/styles/theme';
 
 export function AppointmentDetails() {
   return (
     <Background>
       <Header 
         title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto
+              name="share"
+              size={24}
+              color={theme.colors.primary}
+            />
+          </BorderlessButton>
+        }
       />
     </Background>
   )
