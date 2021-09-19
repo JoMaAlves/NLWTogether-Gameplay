ef3f153d596bb4de01dbf99d078571539bf1dc7b
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-09-19 16:37:52 -0300

    Refactored setLoading()

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -69,13 +69,11 @@ function AuthProvider({ children }: AuthProviderProps) {
           firstName,
           token: params.access_token
         });
-        setLoading(false);
-      } else {
-        setLoading(false);
       }
-
     } catch (error) {
       throw new Error('Nao foi possivel autenticar');
+    } finally {
+      setLoading(false);
     }
   }
 
