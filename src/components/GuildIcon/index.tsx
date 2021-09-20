d0dec6be7c031f1d54bd54a6671dc53d0c390604
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-09-20 01:11:03 -0300

    Loading Guild's Images Dynamically

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,16 +1,36 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, View } from "react-native";
 
 import { styles } from "./styles";
+import DiscordSvg from "../../assets/discord.svg"
 
-export function GuildIcon() {
-  const uri = "https://seeklogo.com/images/D/discord-icon-new-2021-logo-09772BF096-seeklogo.com.png"
+const { CDN_IMAGE } = process.env;
+
+type Props = {
+  guildId: string;
+  iconId: string | null;
+}
+
+export function GuildIcon({ guildId, iconId }: Props) {
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`
+  
+  //"https://seeklogo.com/images/D/discord-icon-new-2021-logo-09772BF096-seeklogo.com.png"
 
   return (
-    <Image 
-      source={{ uri }}
-      style={styles.image}
-      resizeMode="cover"
-    />
+    <View style={styles.container}>
+      {
+        iconId
+        ? <Image 
+            source={{ uri }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        : <DiscordSvg 
+            width={40}
+            height={40}
+          />
+      }
+
+    </View>
   );
 }
--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -72,7 +72,7 @@ export function AppointmentCreate() {
               <View style={styles.select}>
                 {
                   guild.icon
-                  ? <GuildIcon /> 
+                  ? <GuildIcon guildId={guild.id} iconId={guild.icon}/> 
                   : <View style={styles.image}/>
                 }
 
